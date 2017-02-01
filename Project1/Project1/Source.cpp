080aff1bb6ed1b7a85a898da82ee4d2f5ab5b62c
game110pm <game110pm@605-110-STU14>
2017-02-01 13:17:48 -0700

    Second attempt

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,11 +1,11 @@
-#include <iostrream>
+#include <iostream>
 using namespace std;
-
 int main() {
 	char input;//chars hold letters
-	cout<<"Do you like the Broncos?"<< endl;
+	cout << "Do you like the Broncos?" << endl;
 	cin >> input;
 	if (input == 'y')
 		cout << "You da best!" << endl;
 	else if (input == 'n')
 		cout << "Don't talk to me fam" << endl;
+}
